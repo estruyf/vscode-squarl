61b833fa33ba5379da16db9e17ab7d034626cebb
Elio Struyf <elio@struyfconsulting.be>
2022-08-25 12:15:23 +0200

    Update beta release

--- scripts/beta-release.js
+++ scripts/beta-release.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const core = require('@actions/core');
 
 const packageJson = require('../package.json');
 const version = packageJson.version.split('.');
@@ -9,6 +8,5 @@ packageJson.version = `${version[0]}.${version[1]}.${process.argv[process.argv.l
 packageJson.preview = true;
 
 console.log(packageJson.version);
-core.summary.addHeading(`Version info`).addDetails(`${packageJson.version}`);
 
 fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
